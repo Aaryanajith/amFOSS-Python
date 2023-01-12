b98ed436e9705f7eab0b41411f5419c3148f7c13
Aaryan.A <aaryanajith45@gmail.com>
2023-01-12 17:50:23 +0530

    Problems of chapter-4

--- Chapter-4/coin_flip_streaks.py
+++ Chapter-4/coin_flip_streaks.py
@@ -5,7 +5,7 @@ L=[]
 
 numberOfStreaks=0
 streak=0
-for experimentNumber in range(100):
+for experimentNumber in range(10000):
     for i in range(100):
         a=random.choice(R)
         L.append(a)
